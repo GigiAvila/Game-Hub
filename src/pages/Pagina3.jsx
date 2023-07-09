8a2cf90d72c7e17c6e75d40323358b99c57b6fa2
GigiAvila <gigi.s.avila@gmail.com>
2023-07-09 23:17:05 +0200

    changesMemotest

--- src/pages/Pagina3.jsx
+++ src/pages/Pagina3.jsx
@@ -4,85 +4,13 @@ import Modal from '../components/Modal';
 import Loading from '../components/Loading';
 import '../Styles/Memotest.css';
 import blankImg from '../assets/blank.png';
-import facebookImg from '../assets/icons8-facebook.png';
-import githubImg from '../assets/icons8-github.png';
-import instagramImg from '../assets/icons8-instagram.png';
-import linkedinImg from '../assets/icons8-linkedin.png';
-import tiktokImg from '../assets/icons8-tiktok.png';
-import twitterImg from '../assets/icons8-twitter.png';
-import whatsappImg from '../assets/icons8-whatsapp.png';
-import youtubeImg from '../assets/icons8-youtube.png';
+import memotestArray from '../data/MemotestData';
 
 const Pagina3 = () => {
-  const memotestItem = [
-    {
-      name: 'facebook',
-      image: facebookImg,
-    },
-    {
-      name: 'github',
-      image: githubImg,
-    },
-    {
-      name: 'instagram',
-      image: instagramImg,
-    },
-    {
-      name: 'linkedin',
-      image: linkedinImg,
-    },
-    {
-      name: 'tiktok',
-      image: tiktokImg,
-    },
-    {
-      name: 'twitter',
-      image: twitterImg,
-    },
-    {
-      name: 'whatsapp',
-      image: whatsappImg,
-    },
-    {
-      name: 'youtube',
-      image: youtubeImg,
-    },
-    {
-      name: 'facebook',
-      image: facebookImg,
-    },
-    {
-      name: 'github',
-      image: githubImg,
-    },
-    {
-      name: 'instagram',
-      image: instagramImg,
-    },
-    {
-      name: 'linkedin',
-      image: linkedinImg,
-    },
-    {
-      name: 'tiktok',
-      image: tiktokImg,
-    },
-    {
-      name: 'twitter',
-      image: twitterImg,
-    },
-    {
-      name: 'whatsapp',
-      image: whatsappImg,
-    },
-    {
-      name: 'youtube',
-      image: youtubeImg,
-    }
-  ];
+  const memotestItem = memotestArray;
 
   const [isLoading, setIsLoading] = useState(true);
-  const [blankmemotestItem, setmMemotestItem] = useState([]);
+  const [memotestItems, setMemotestItems] = useState([]);
   const [selectedItems, setSelectedItems] = useState([]);
   const [matchedItems, setMatchedItems] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
@@ -90,17 +18,15 @@ const Pagina3 = () => {
   const [allMatchesFound, setAllMatchesFound] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => {
-      const shuffledItems = shuffleArray(memotestItem);
-      const updatedItems = shuffledItems.map((item, index) => ({
-        ...item,
-        originalImage: item.image,
-        image: blankImg,
-        id: index + 1
-      }));
-      setmMemotestItem(updatedItems);
-      setIsLoading(false);
-    }, 1500);
+    const shuffledItems = shuffleArray(memotestItem);
+    const initialItems = shuffledItems.map((item, index) => ({
+      ...item,
+      originalImage: item.image,
+      image: blankImg,
+      id: index + 1
+    }));
+    setMemotestItems(initialItems);
+    setIsLoading(false);
   }, []);
 
   const shuffleArray = (array) => {
@@ -113,10 +39,11 @@ const Pagina3 = () => {
   };
 
   const flipCard = (card) => {
-    if (matchedItems.includes(card) || isCheckingMatch) {
+    if (matchedItems.includes(card) || selectedItems.length >= 2) {
       return;
     }
-    const itemsWithUpdatedImg = blankmemotestItem.map((item) => {
+
+    const updatedItems = memotestItems.map((item) => {
       if (item.id === card.id) {
         const newImage = item.image === blankImg ? item.originalImage : blankImg;
         return {
@@ -126,25 +53,40 @@ const Pagina3 = () => {
       }
       return item;
     });
-    setmMemotestItem(itemsWithUpdatedImg);
-    setSelectedItems((prevItems) => [...prevItems, card]);
+
+    setMemotestItems(updatedItems);
+
+    const updatedSelectedItems = [...selectedItems, card];
+    setSelectedItems(updatedSelectedItems);
+
+    if (updatedSelectedItems.length === 2) {
+      setIsCheckingMatch(true);
+    }
   };
 
   useEffect(() => {
-    if (selectedItems.length >= 2) {
-      setIsCheckingMatch(true);
-    }
-  }, [selectedItems]);
-
-  useEffect(() => {
     const checkForMatch = () => {
-      const [firstItem, secondItem] = selectedItems.slice(-2);
-      if (firstItem.name === secondItem.name) {
+      const [firstItem, secondItem] = selectedItems;
+
+      if (firstItem && secondItem && firstItem.name === secondItem.name) {
         setMatchedItems((prevItems) => [...prevItems, firstItem, secondItem]);
         setModalVisible(true);
+
+        const updatedItems = memotestItems.map((item) => {
+          if (item.id === firstItem.id || item.id === secondItem.id) {
+            return {
+              ...item,
+              image: item.originalImage // Aquí asignamos la imagen original en lugar de blankImg
+            };
+          }
+          return item;
+        });
+
+        setMemotestItems(updatedItems);
+        setSelectedItems([]);
       } else {
         setTimeout(() => {
-          const updatedItems = blankmemotestItem.map((item) => {
+          const updatedItems = memotestItems.map((item) => {
             if (item.id === firstItem.id || item.id === secondItem.id) {
               return {
                 ...item,
@@ -153,17 +95,19 @@ const Pagina3 = () => {
             }
             return item;
           });
-          setmMemotestItem(updatedItems);
+
+          setMemotestItems(updatedItems);
+          setSelectedItems([]);
         }, 1500);
       }
-      setSelectedItems([]);
-      setIsCheckingMatch(false);
     };
 
     if (isCheckingMatch) {
       checkForMatch();
+      setIsCheckingMatch(false);
     }
-  }, [isCheckingMatch]);
+  }, [isCheckingMatch, selectedItems]);
+
 
   useEffect(() => {
     if (matchedItems.length === memotestItem.length) {
@@ -172,12 +116,18 @@ const Pagina3 = () => {
   }, [matchedItems, memotestItem]);
 
   const handleResetGame = () => {
-    setmMemotestItem([]);
+    const shuffledItems = shuffleArray(memotestItem);
+    const updatedItems = shuffledItems.map((item, index) => ({
+      ...item,
+      originalImage: item.image,
+      image: blankImg,
+      id: index + 1
+    }));
+    setMemotestItems(updatedItems);
     setSelectedItems([]);
     setMatchedItems([]);
     setModalVisible(false);
     setAllMatchesFound(false);
-    setIsLoading(true);
   };
 
   return (
@@ -186,13 +136,13 @@ const Pagina3 = () => {
       {isLoading ? (
         <Loading />
       ) : (
-        <div className='memotestBody'>
-          <h3 className='memotestTitle'> MEMOTEST </h3>
+        <div className="memotestBody">
+          <h3 className="memotestTitle"> MEMOTEST </h3>
           <button className="resetButton" onClick={handleResetGame}>
             Volver a jugar
           </button>
-          <div className='grid'>
-            {blankmemotestItem.map((card) => (
+          <div className="grid">
+            {memotestItems.map((card) => (
               <div key={card.id} onClick={() => flipCard(card)}>
                 <img src={card.image} alt={card.name} />
               </div>
@@ -200,10 +150,8 @@ const Pagina3 = () => {
           </div>
         </div>
       )}
-      {modalVisible && <Modal message="¡Muy bien! Ya entendiste de que va sigue así!" />}
-      {allMatchesFound && (
-        <Modal message="¡Felicitaciones! ¡Encontraste todas las coincidencias!" />
-      )}
+      {modalVisible && <Modal message="¡Muy bien! Ya entendiste de qué va, ¡sigue así!" />}
+      {allMatchesFound && <Modal message="¡Felicitaciones! ¡Encontraste todas las coincidencias!" />}
     </>
   );
 };
